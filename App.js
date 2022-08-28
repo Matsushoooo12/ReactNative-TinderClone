37f80fe55ed0469a055321cc3031dbd7fe9b095f
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-08-29 01:23:06 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useTailwind } from "tailwind-rn";
+import { AuthProvider } from "./hooks/useAuth";
+import StackNavigator from "./StackNavigator";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
